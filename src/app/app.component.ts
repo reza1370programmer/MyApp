13ec77ac56ee0c5bf4f6c81b880e83b09fa120d1
reza1370programmer <78476427+reza1370programmer@users.noreply.github.com>
2024-07-25 15:26:09 +0330

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MinLengthDirective } from './min-length.directive';
+import { DatePipe, DecimalPipe, JsonPipe } from '@angular/common';
+import { MypipePipe } from './pipes/mypipe.pipe';
+import { MyserviceService } from './services/myservice.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  providers: [MyserviceService],
+  imports: [RouterOutlet, DatePipe, MinLengthDirective, JsonPipe, DecimalPipe, MypipePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'MyApp';
+  counter = 0
+  constructor(public myService: MyserviceService) {
+  }
+  
+  @HostListener('window:keydown.enter', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    this.counter++;
+  }
+
+  title = 'this is my app';
+  myDate = new Date();
+  myJson = { firstname: 'reza', lastname: 'ahmadi', age: 30 };
+  myNumber = 6498133989756;
+  // myObject = { firstname: 'ali', lastname: 'alizade', average: 15 }
+  values: string[] = ['apple', 'banana', 'carrot', 'date'];
+
+  addToService(num: string) {
+    this.myService.myArray.push(num)
+  }
 }
