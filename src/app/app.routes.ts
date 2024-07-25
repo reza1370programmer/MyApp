7a81eee114e808e20db6f3b2d74b47e7b5b8f52f
reza1370programmer <78476427+reza1370programmer@users.noreply.github.com>
2024-07-25 15:38:49 +0330

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// export const routes: Routes = [
+//     { path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard] },
+//     { path: 'cart', component: CartDetailComponent, canActivate: [StoreFirstGuard] },
+//     { path: 'checkout', component: CheckoutComponent, canActivate: [StoreFirstGuard] },
+//     {
+//         path: 'admin', canActivate: [AuthGuard], children: [
+//             { path: 'main', component: AdminComponent, },
+//             { path: "products/:mode/:id", component: ProductEditorComponent },
+//             { path: "products/:mode", component: ProductEditorComponent },
+//             { path: "products", component: ProductTableComponent },
+//             { path: "orders", component: OrderTableComponent },
+//             { path: "**", redirectTo: "main" }
+//         ]
+//     },
+//     {
+//         path: 'auth', component: AuthComponent
+//     },
+//     { path: '**', redirectTo: '/store' }
+// ];
